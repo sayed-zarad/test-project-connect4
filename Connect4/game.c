94f4a049270c15d23f9120ee23646df648f3cc2b
afafabdelwahab <113607838+afafabdelwahab@users.noreply.github.com>
2023-05-19 22:14:10 +0300

    Add files via upload

---
+++ Connect4/game.c
@@ -0,0 +1,30 @@
+#include "Game.h"
+volatile int col = 0;
+volatile int g;
+volatile int sw1_pressed = 0;
+volatile int sw2_pressed = 0;
+
+
+void startingScreen()
+{
+	int w; 
+	Nokia5110_SetCursor(0,0);
+	Nokia5110_ClearBuffer();
+	
+	Nokia5110_DisplayBuffer();
+	Nokia5110_SetCursor(2,2);
+	Nokia5110_OutString("Welcome!  connect4");
+
+		
+	TIMER_INIT( 2,20,"Seconds","periodic","32");
+	for(w=0 ; w<7 ; w++)
+	{
+		Nokia5110_SetCursor(0,w);
+		Nokia5110_OutString("           ");
+	TIMER_INIT( 2,40,"Seconds","periodic","32");
+	}
+
+}
+
+
+
