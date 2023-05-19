bd0c27dd3cad8d197444514883100cd01f319430
alaa5radwan <66218719+alaa5radwan@users.noreply.github.com>
2023-05-19 22:27:55 +0300

    Update Game.h

--- Connect4/Game.h
+++ Connect4/Game.h
@@ -16,7 +16,14 @@ extern volatile int sw2_pressed ;
 
 void printBoard(char *board);
 int takeTurn(char *board, int player, const char*);
-
+int hasEmptyCol(char *board, int col  ); 
+int changeBoard(char *board,int player , const char *PIECES , int col );
+int checkWin(char *board);
+int checkFour(char *board, int, int, int, int);
+int horizontalCheck(char *board);
+int verticalCheck(char *board);
+int diagonalCheck(char *board);
+void startingScreen(void);
 
 
 
